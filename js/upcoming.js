cd9f286867a056f35392255d2e89efe9edaa478a
Shannon Ding <shannon_ding@yahoo.com>
2019-01-08 22:33:05 -0800

    sta and pillow

--- js/upcoming.js
+++ js/upcoming.js
@@ -115,35 +115,35 @@ var eventsList = [
 		"desc": '<p>Unwind and watch a movie on the patio! There will be popcorn for sale for $2 to fundraise for upcoming NYIM events. <a href="https://docs.google.com/forms/d/1sD5rj_rplOzuSiTKYhpkXHkjmV1V8yykNTxOg1LJuio/edit" target="_blank">RSVP here</a>. Come in comfy clothes and bring a blanket!</p>',
 		"type": types.all,
 		"tag": "movies",
-		"visible": true
+		"visible": false
 	},
 	{
 		"name": "Liaison Meeting",
 		"date": "November 18 (Sunday) 10-11am",
 		"desc": '<p>One officer from each club must attend.</p>',
 		"type": types.officers,
-		"visible": true
+		"visible": false
 	},
 	{
 		"name": "Blood Donor Card Making",
 		"date": "November 18 (Sunday) 11am-12pm",
 		"desc": '<p>Come out to the chapter from 11am-12pm to make thank-you cards for our wonderful blood donors to receive during the holiday time!  Supplies will be provided but feel free to bring your own (there will be food too!). <a href="https://docs.google.com/forms/d/e/1FAIpQLSdpyWOzwoL4aFI98Q6a3AJOoZ_VDZ48-piG0S5AmZOjjA-eDQ/viewform" target="_blank">Sign up here.</a></p>',
 		"type": types.all,
-		"visible": true
+		"visible": false
 	},
 	{
 		"name": "CDE Training",
 		"date": "November 18 (Sunday) 11am-12:30pm",
 		"desc": '<p>Come to the chapter in meeting room 2 to be CDE trained and get the chance to present with an Americorp! <a href="https://docs.google.com/forms/d/e/1FAIpQLSf8OvnZvPHgoOmC15R9GGji20pKv9Gzn6LjLqNT2dNFBJjsvg/viewform" target="_blank">Sign up here.</a></p>',
 		"type": types.all,
-		"visible": true
+		"visible": false
 	},
 	{
 		"name": "Health and Safety Training",
 		"date": "November 25 (Sunday) 9:30am-2pm",
 		"desc": '<p>Become a Health and Safety Instructor through this training at the chapter! Learn about first aid and CPR topics. This is open to all youth and lunch is provided. Limited spots so <a href="https://docs.google.com/forms/d/e/1FAIpQLSctDSxlLhKRXDB93YdtWp8twUCHvbviJPSJ2VbrsGrlgclh_g/viewform" target="_blank">sign up here soon</a>! (An email will be sent indicating if there is space in the class for you.)</p>',
 		"type": types.all,
-		"visible": true
+		"visible": false
 	},
 	{
 		"name": "Totes of Hope Packaging Event",
@@ -151,12 +151,40 @@ var eventsList = [
 		"desc": '<p>Over the entire month of November, we are collecting donations for <b>Totes of Hope</b>, a personal care kit drive for homeless veterans. Bring your donations to the Youth Services room in the chapter, or collect donations within your club and have an officer bring them all. On <b>November 30th</b>, we will host a packaging event at the chapter before sending our totes off to veterans hospitals and outreach programs! <a href="https://docs.google.com/forms/d/1NqSMHWvA5FBKImZ8DG_zw_j18UzxSz9s6ONl9GoovAE/edit" target="_blank">Sign up for the packaging event</a> and <a href="newsletters/11.5-18.html#totes" target="_blank">learn more on how you and your club can help</a>.</p>',
 		"type": types.all,
 		"tag": "totes",
-		"visible": true
+		"visible": false
 	},
 	{
 		"name": "Sound the Alarm",
 		"date": "December 1 (Saturday) 8:30am-2:30pm",
 		"desc": '<p>Help us #EndHomeFires by installing smoke alarms and providing fire safety education within your community. Our Sound the Alarm events are generally held one Saturday every month. There is no age limit to participate.</p><p>The next event is <b>December 1st (Saturday)</b> at the County Fair Mobile Estate. Meet at <b>San Jose Fire Station 24</b>: 1924 Yerba Buena Rd, San Jose, CA, 95121. Sign up at <a href="http://tiny.cc/STADec1" target="_blank">http://tiny.cc/STADec1</a> and contact <b>rcdisastercommittee@gmail.com</b> for any questions.</p>',
+		"type": types.all,
+		"visible": false
+	},
+	{
+		"name": "Sound the Alarm Canvassing",
+		"date": "January 12 (Saturday) 10am-12pm",
+		"desc": '<p>Raise awareness of our <b>January 19th</b> Sound the Alarm in the La Buona Vita Mobile Home Community by canvassing. Volunteers will go house to house talking to residents and distributing doorhangers. Sign up link TBA.</p>',
+		"type": types.all,
+		"visible": true
+	},
+	{
+		"name": "Sound the Alarm",
+		"date": "January 19 (Saturday) 8:30am-3pm",
+		"desc": '<p>Help us #EndHomeFires by installing smoke alarms and providing fire safety education within your community. Our Sound the Alarm events are generally held one Saturday every month. There is no age restriction to participate.</p><p>The next event is <b>January 19th (Saturday)</b> at La Buona Vita Mobile Home Community. Meeting location and sign up links TBA. Contact <b>rcdisastercommittee@gmail.com</b> for any questions.</p>',
+		"type": types.all,
+		"visible": true
+	},
+	{
+		"name": "Liaison Meeting",
+		"date": "January 20 (Sunday) 10-11am",
+		"desc": '<p>One officer from each club must attend.</p>',
+		"type": types.officers,
+		"visible": false
+	},
+	{
+		"name": "Pillowcase Project Training",
+		"date": "January 27 (Sunday) 11am-3pm",
+		"desc": '<p>This training is at the Silicon Valley Red Cross chapter and open to all Red Cross youth. We <b>highly recommend</b> that you attend. Food/lunch will be provided!<br>The Pillowcase Project is an interactive preparedness program designed for youth grades 3 to 5 which aims to increase understanding of natural hazards and teaches safety and personal preparedness. Becoming trained will allow you to teach these classes along with our Americorps and other youth volunteers in the local community.<br>Please <b>RSVP by January 23rd</b> for the training <a href="https://docs.google.com/forms/d/e/1FAIpQLSfo2zZmCS7Qg3ni_tRFKeVfIQsB0mVDbx6M5-KrrG4btLuJmQ/viewform?usp=send_form" target="_blank">here</a>.<br>Before you attend the training, however, you must <b>complete the following online modules</b>: "Basic Instructor Fundamentals for Youth" and the "Pillowcase Project Presenter Fundamentals Module 1" We have attached a document <a href="https://docs.google.com/document/d/10hBX6C46ZE-BKoC_l_JO78ggZesVN14QF17eFlgCP7Q/edit" target="_blank">here</a> with full instructions and screenshots to make the process easier.<br>Email <i>oishika.barat@redcross.org</i> if you have questions.</p>',
 		"type": types.all,
 		"visible": true
 	}
